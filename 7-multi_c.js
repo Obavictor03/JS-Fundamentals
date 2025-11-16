c316be152b2178c3355c8ebab924a23d1d82c8f7
Obavictor03 <obarovictor03@gmail.com>
2025-11-16 16:43:52 +0100

    add function

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -5,7 +5,7 @@ const x = Math.floor(Number(arg))
 if (isNaN(x)) {
     console.log("Missing number of occurrences")
 } else {
-    for(i = 0; i < x; i++) {
+    for (i = 0; i < x; i++) {
         console.log("C is fun")
     }
 }
---
+++ 9-add.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+    return a + b
+}
+
+const a = Math.floor(Number(process.argv[2]))
+const b = Math.floor(Number(process.argv[3]))
+
+if(isNaN(a) || isNaN(b)) {
+    console.log("NaN")
+} else {
+    console.log(add(a, b))
+}
